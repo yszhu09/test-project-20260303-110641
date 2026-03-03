25360b681eee5519d54cc2ed257d0b19b896313e
yszhu09 <335808046@qq.com>
2026-03-03 21:04:23 +0800

    feat: 添加 subtract 减法函数
    - 添加 subtract(a, b) 函数用于减法运算
    - 更新 module.exports 导出 subtract
    - 修复 issue #1
    
    Closes #1

--- src/utils.js
+++ src/utils.js
@@ -6,10 +6,14 @@ function multiply(a, b) {
   return a * b;
 }
 
+function subtract(a, b) {
+  return a - b;
+}
+
 function capitalize(str) {
   // 修复：处理空字符串和 null/undefined
   if (!str || str.length === 0) return '';
   return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
-module.exports = { add, multiply, capitalize };
+module.exports = { add, multiply, subtract, capitalize };
